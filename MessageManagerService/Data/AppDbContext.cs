062fc7f99e7df8bb303223cc14173221875f7bd2
idan shnitzki <idanshani10@gmail.com>
2021-11-22 18:46:55 +0200

    added DbContext - InMemoryDatabase

---
+++ MessageManagerService/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MessageManagerService.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageManagerService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+    }
+}
